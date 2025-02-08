0d3008f0dcdaad7910119fc154a3bbd53052c522
WilliamOHara <william.ohara@subscripify.com>
2025-02-08 15:19:50 -0800

    push

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/subscripify-private/go-teach
 
 go 1.23.4
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	mux "github.com/gorilla/mux"
+)
+
+type Message struct {
+	Message string `json:"message"`
+}
+
+type ResponseMessage struct {
+	Message     string  `json:"message"`
+	YourMessage Message `json:"your_message"`
+}
+
 func main() {
-	println("Hello World")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/message", messageHandler).Methods("GET")
+	r.HandleFunc("/echo", echoHandler).Methods("POST")
+	log.Println("Server starting on port 8080...")
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatalf("could not start listener %v", err.Error())
+	}
+
+}
+
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	response := Message{Message: "Hello World"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	var msg Message
+	if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+	response := ResponseMessage{
+		Message:     "I processed your message",
+		YourMessage: msg,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
