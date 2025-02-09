911cc76e73f5979b73f98200b24e8f6b354409b0
William O'Hara <william.ohara@subscripify.com>
2025-02-08 16:42:14 -0800

    step 1 created

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"sync"
@@ -39,64 +37,9 @@ func main() {
 }
 
 func colorRetriever(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Request to get color received")
-	color, err := mux.Vars(r)["color"]
 
-	if !err {
-		response := ResponseMessage{
-			Message: "Invalid request",
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-
-	fmt.Printf("getting %s \n", color)
-	for _, c := range dataStore {
-		if c == color {
-			response := ResponseMessage{
-				Message: color + " is a nice color",
-				Color:   color,
-			}
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(response)
-			return
-		}
-	}
-
-	response := ResponseMessage{
-		Message: color + " is not found",
-		Color:   color,
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	json.NewEncoder(w).Encode(response)
 }
 
 func colorMaker(w http.ResponseWriter, r *http.Request) {
-	var request RequestMessage
-	defer r.Body.Close()
 
-	fmt.Println("Request to create color received")
-
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		response := ResponseMessage{
-			Message: "Invalid request",
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-	fmt.Printf("making %s \n", request.Color)
-	dataStore = append(dataStore, request.Color)
-	response := ResponseMessage{
-		Message: "I processed your message",
-		Color:   request.Color,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
 }
