f6f8572d752af9918359f5ff16a7fe4e61cdbdb7
William O'Hara <william.ohara@subscripify.com>
2025-02-09 07:12:38 -0800

    added post

--- main.go
+++ main.go
@@ -53,9 +53,6 @@ func colorRetriever(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	mu.Lock()
-	defer mu.Unlock()
-
 	fmt.Printf("getting %s \n", color)
 	for _, c := range dataStore {
 		if c == color {
@@ -95,9 +92,6 @@ func colorMaker(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Printf("making %s \n", request.Color)
-
-	mu.Lock()
-	defer mu.Unlock()
 
 	dataStore = append(dataStore, request.Color)
 	response := ResponseMessage{
